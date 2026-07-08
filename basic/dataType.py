f5a97400246a6bb69dfd1852ab8ae1ea3096d92f
somafu <omafuoloche4@gmail.com>
2026-07-08 11:59:03 +0100

    data type

---
+++ basic/dataType.py
@@ -0,0 +1,18 @@
+# DATA TYPES
+
+# str
+# this is how to declare a multiple line string in python 
+funny_quote = """Jesus you are the mighty man of war
+Jesus you will never ever fall
+when you say it, you will do it"""
+print(funny_quote)
+
+# float 
+# when you do / between two number it return a float output auto..ly, but if you whan it to return an interger you do this //
+
+subj_num = 3
+
+total_score = 80 + 90 + 92
+average_score =  total_score / subj_num
+
+print(average_score)
