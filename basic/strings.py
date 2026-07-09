49da14eee3f55017d79fdb93cffb31ebf2c1b17a
somafu <omafuoloche4@gmail.com>
2026-07-09 15:00:32 +0100

    check string

---
+++ basic/strings.py
@@ -0,0 +1,22 @@
+# Check string
+
+# to check if the a particular word or phrase is in a string use the 'in' keyword
+# 
+
+# example
+txt = "I love God, and his love reigns for ever"
+print("God" in txt)
+
+
+fruit = ["banana","","orange"]
+
+if "apple" in fruit:
+    print("apple of my eye")
+    print("apple" in fruit)
+else:
+    print("omo apple no dey oo")
+    print("apple" in fruit)
+
+# you can also check if a particular word or phrase is not in a sentence, using the "not" and "in" key word together
+
+print("apple" not in fruit)# true because there is no apple in the fruit list.
