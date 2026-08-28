231100ba705110998697739063c5ae1ebcd7a2ba
moochekwu <unclemoerice@gmail.com>
2026-08-28 17:54:06 +0100

    sortlist

---
+++ execise/sortlist.py
@@ -0,0 +1,28 @@
+## 
+
+def smallest_Item(arr):
+
+    smallest = arr[0]
+    smallest_Index = 0
+
+    for i in range(1, len(arr)):
+
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_Index = i
+    return smallest
+
+def sortlist(arr):
+
+    Newlist = []
+
+    for i in range(len(arr)):
+        smallestnum  = smallest_Item(arr)
+        Newlist.append(smallest)
+        arr.remove(smallest)
+    return Newlist
+
+sml = [4,9,7,2,5,8]
+
+print(smallest_Item(sml))
+print(sortlist(sml))
