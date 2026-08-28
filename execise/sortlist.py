c623642d721c3c3c3a6e7eff89bc4b3df2afac5f
moochekwu <unclemoerice@gmail.com>
2026-08-28 22:59:51 +0100

    rebuild

--- execise/sortlist.py
+++ execise/sortlist.py
@@ -1,4 +1,5 @@
-## before writing the sort_list function, we need a helper function smallest_item function that will pick the smallest number in a list
+# before writing the sort_list function, we need a helper function smallest_item function that will pick the smallest number in a list
+
 def smallest_Item(arr):
 
     smallest = arr[0]
@@ -7,18 +8,23 @@ def smallest_Item(arr):
     for i in range(1, len(arr)):
 
         if arr[i] < smallest:
+            
             smallest = arr[i]
             smallest_Index = i
+
     return smallest
+
 
 def sortlist(arr):
 
     Newlist = []
 
     for i in range(len(arr)):
+
         smallestnum  = smallest_Item(arr)
         Newlist.append(smallest)
         arr.remove(smallest)
+
     return Newlist
 
 sml = [4,9,7,2,5,8]
